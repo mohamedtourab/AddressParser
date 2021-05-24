8cebb2a7650fd2f802ee64bb6a434baf88eb40db
Mohamed Tourab <mohamedmtourab@gmail.com>
2021-05-24 19:12:07 +0200

    Handling having No and Nr inside the address

--- src/main/java/com/example/addressline/service/AddressLineService.java
+++ src/main/java/com/example/addressline/service/AddressLineService.java
@@ -1,10 +1,15 @@
 package com.example.addressline.service;
 
 import com.example.addressline.dao.DetailedAddress;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Stack;
 
 @Service
 public class AddressLineService {
@@ -14,28 +19,62 @@ public class AddressLineService {
     public String parseAddress(String address) {
         address = address.replaceAll("\\p{Punct}", "");
         String addressJson = invalidAddress;
-        if (startsWithNumber(address)) {
+        DetailedAddress detailedAddress = null;
+        //TODO check if string 'no' or 'nr' if so we split on it create the json.
+        if (address.toLowerCase(Locale.ROOT).contains("no") || address.toLowerCase(Locale.ROOT).contains("nr")) {
+            String[] splicedString = address.toLowerCase(Locale.ROOT).split("nr|no");
+            detailedAddress = new DetailedAddress(splicedString[0], splicedString[1]);
             try {
-                DetailedAddress detailedAddress = new DetailedAddress(address.substring(address.indexOf(' ') + 1), address.substring(0, address.indexOf(' ')));
-                ObjectMapper objectMapper = new ObjectMapper();
-                addressJson = objectMapper.writeValueAsString(detailedAddress);
+                addressJson = jsonifyObject(detailedAddress);
                 logger.info("Successfully parsed the address");
             } catch (Exception e) {
                 logger.error("Failed to create json string");
             }
         } else {
-            try {
-                ObjectMapper objectMapper = new ObjectMapper();
-                DetailedAddress detailedAddress = new DetailedAddress("test", "test");
-                addressJson = objectMapper.writeValueAsString(detailedAddress);
-            } catch (Exception e) {
-                logger.error("Failed to create json string");
+            if (startsWithNumber(address)) {
+                try {
+                    detailedAddress = new DetailedAddress(address.substring(address.indexOf(' ') + 1), address.substring(0, address.indexOf(' ')));
+                    addressJson = jsonifyObject(detailedAddress);
+                    logger.info("Successfully parsed the address");
+                } catch (Exception e) {
+                    logger.error("Failed to create json string");
+                }
+            } else {
+                try {
+                    String[] splitAddress = address.split("\\s+");
+                    Stack<String> stringsStack = new Stack<>();
+                    for (String s : splitAddress) {
+                        if (isNumber(s)) {
+                            stringsStack.pop();
+                            StringBuilder stringBuilder = new StringBuilder();
+                            stringsStack.forEach(stringBuilder::append);
+                            detailedAddress = new DetailedAddress(stringBuilder.toString(), s);
+                        } else {
+                            stringsStack.push(s);
+                        }
+                    }
+                    addressJson = jsonifyObject(Objects.requireNonNull(detailedAddress));
+                } catch (Exception e) {
+                    logger.error("Failed to create json string");
+                }
             }
         }
+
+
         return addressJson;
     }
 
     private boolean startsWithNumber(String address) {
         return address.split("\\s+")[0].chars().allMatch(Character::isDigit);
     }
+
+    private String jsonifyObject(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+
+    private boolean isNumber(String s) {
+        return s.chars().allMatch(Character::isDigit);
+    }
 }
+
